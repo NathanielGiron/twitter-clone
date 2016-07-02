d42f4eb47ea5aa6179ed068ac576d7f333cb4cae
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-07-02 15:44:43 -0700

    added style to homepage, created registration page

--- public/js/tweetApp.js
+++ public/js/tweetApp.js
@@ -10,3 +10,15 @@ app.controller('mainController', function($scope) {
 		$scope.newPost = {created_by: '', text: '', created_at: ''};
 	}
 });
+
+app.controller('authController', function($scope) {
+	$scope.user = {username: '', password: ''};
+	$scope.error_message = '';
+
+	$scope.login = function() {
+		$scope.error_message = 'login request for ' + $scope.user.username;
+	}
+	$scope.register = function() {
+		$scope.error_message = 'registration request for ' + $scope.user.username;
+	}
+});
